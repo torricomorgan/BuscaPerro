17428b1da91653e81f6bb3634f9fc370c36f11a3
Matias <matias.dtm@gmail.com>
2022-06-11 11:25:22 -0400

    Cambios

---
+++ FormMascota/FormMascota/wwwroot/js/map.js
@@ -0,0 +1,19 @@
+﻿
+var map;
+
+function initialize(lat, lng, datos) {
+    var latlng = new google.maps.LatLng(lat, lng);
+    var options = {
+        zoom: 14, center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var marker = new google.maps.Marker({
+        position: latlng,
+        title: "Mascota - " + datos
+    });
+
+    map = new google.maps.Map(document.getElementById("map"), options);
+    marker.setMap(map);
+}
+
